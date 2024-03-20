3ed47de3d58d9bf56666ea2423649867febd64d5
zbxuli <ZhongboLi2020@outlook.com>
2024-03-20 20:48:14 +1100

    Initial commit

---
+++ src/severalFor.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class severalFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double allAveGrade =0;
+
+        for (int i = 0; i < 3; i++) {
+            double grades = 0;
+            int count=0;
+            double individualGrades = 0;
+            double averageGrade = 0;
+            for (int j = 0; j < 5; j++) {
+                individualGrades =sc.nextDouble();
+                grades += individualGrades;
+                if(individualGrades >= 60){
+                    count++;
+                }
+
+            }
+            System.out.println("及格人数" + count);
+            averageGrade =grades/5;
+            allAveGrade +=grades;
+            System.out.println(averageGrade);
+        }
+        System.out.println(allAveGrade/15);
+    }
+
+
+}
