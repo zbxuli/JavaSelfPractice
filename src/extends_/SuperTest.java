ae0f0d4f779ea18699a34fcb6ec1fa71609e8a4d
zbxuli <ZhongboLi2020@outlook.com>
2024-04-08 19:54:38 +1000

    a example for super() practice

---
+++ src/extends_/SuperTest.java
@@ -0,0 +1,11 @@
+package extends_;
+
+public class SuperTest {
+    public static void main(String[] args) {
+        B b = new B();
+        b.hi();
+        b.hello();
+        b.sum();
+
+    }
+}
